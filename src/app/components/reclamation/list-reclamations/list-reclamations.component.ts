bfc3f856940998c61b8124bd03aa1f131766f3cb
Ameni-BenJeddou <amanibenjeddou.abj@gmail.com>
2023-07-21 00:56:13 +0100

    integrate pdfmaker api

--- src/app/components/reclamation/list-reclamations/list-reclamations.component.ts
+++ src/app/components/reclamation/list-reclamations/list-reclamations.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Reclamation } from 'src/app/Entity/Reclamation';
 import { ReclamationService } from 'src/app/services/reclamation.service';
+import pdfMake from "pdfmake/build/pdfmake";  
+import pdfFonts from "pdfmake/build/vfs_fonts";  
+pdfMake.vfs = pdfFonts.pdfMake.vfs;   
 
 @Component({
   selector: 'app-list-reclamations',
@@ -20,6 +23,83 @@ export class ListReclamationsComponent {
   deleteReclamation(id: number) {
     this.service.DeleteReclamation(id).subscribe(p => {
       console.log('delete');
+      window.location.reload();
     });
+  }
+  generatePDF(rec: Reclamation) {
+    let docDefinition = {
+      content: [
+        {
+          text: 'Immeuble application',
+          fontSize: 16,
+          alignment: 'center',
+          color: '#047886'
+        },
+        {
+          text: 'Reclamation',
+          fontSize: 20,
+          bold: true,
+          alignment: 'center',
+          decoration: 'underline',
+          color: 'skyblue'
+        },
+        {
+          text: 'User Details',
+          style: 'sectionHeader'
+        },
+        {
+          columns: [
+            [
+              {
+                text: rec.claimer.fullName,
+                bold:true
+              }
+            ],
+            [
+              {
+                text: `Date: ${new Date().toLocaleString()}`,
+                alignment: 'right'
+              },
+              { 
+                text: `Bill No : ${rec.id}`,
+                alignment: 'right'
+              }
+            ]
+          ]
+        },
+        {text: 'Title : '+rec.title},
+        {text: 'Description : '+rec.description},
+        {text: 'Is is treated? : '+rec.treated},
+        {text: 'Created at : '+rec.creationDate},
+        {text: 'Solution : '+rec.solution},
+        {
+          columns: [
+            [{ qr: `${rec.claimer.fullName}`, fit: '50' }],
+            [{ text: 'Signature', alignment: 'right', italics: true}],
+          ]
+        },
+        {
+          text: 'Terms and Conditions',
+          style: 'sectionHeader'
+        },
+        {
+            ul: [
+              'Reclamation are property of the application.',
+              'Failiure to comply with terms and conditions will result in immidiate death.',
+              'This is system generated Reclamation never trust the machine.',
+              'Hakuna Matata',
+            ],
+        }
+      ],
+      styles: {
+        sectionHeader: {
+          bold: true,
+          decoration: 'underline',
+          fontSize: 14,
+          margin: [0, 15,0, 15]          
+        }
+      }
+    };
+    pdfMake.createPdf(docDefinition).open();      
   }
 }
